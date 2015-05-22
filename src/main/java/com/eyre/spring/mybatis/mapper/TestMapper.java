61139b1b201f15b744d66ed75a8c026618d63835
Administrator <wei_ai_bao@sina.com>
2015-05-22 11:32:19 +0800

    去除mybatis 缓存功能  修改mybatis maven 配置  使其打包生成mapper.xml  文件

--- src/main/java/com/eyre/spring/mybatis/mapper/TestMapper.java
+++ src/main/java/com/eyre/spring/mybatis/mapper/TestMapper.java
@@ -4,9 +4,14 @@ import com.eyre.spring.mybatis.entity.Test;
 import com.github.abel533.mapper.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2015/5/18.
  */
 @Repository
 public interface TestMapper extends Mapper<Test> {
+
+    List<Test> selectAll();
+
 }
